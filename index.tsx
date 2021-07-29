4b424359c76a4bbbb3f7da13f7a0fef916cc0185
Khongchai Greesuradej <56601823+Khongchai@users.noreply.github.com>
2021-07-30 01:02:09 +0700

    Added monitorScroll function

--- index.tsx
+++ index.tsx
@@ -105,4 +105,32 @@ const Sidebar: React.FC<SidebarProps> = ({
   );
 };
 
+function monitorScroll(mainElems: HTMLCollectionOf<Element>) {
+  const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.6,
+  };
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const id = entry.target.innerHTML + "-sidebar-item";
+        const sidenavItem = document.getElementById(id);
+        sidenavItem.classList.add("sidebar-inview");
+      } else {
+        const id = entry.target.innerHTML + "-sidebar-item";
+        const sidenavItem = document.getElementById(id);
+        sidenavItem.classList.remove("sidebar-inview");
+      }
+    });
+  }, observerOptions);
+
+  for (let i = 0, length = mainElems.length; i < length; i++) {
+    if (mainElems[i]) {
+      observer.observe(mainElems[i]);
+    }
+  }
+}
+
+
 export default Sidebar;
